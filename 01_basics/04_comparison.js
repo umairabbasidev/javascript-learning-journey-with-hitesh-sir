788490bce272a2d6196799d61b386dd8782966b0
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-05 21:17:05 +0500

    understanding comparison opertor in js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1) // true
+// console.log(5>=8)  // false
+// console.log(2<1) // false
+// console.log(2<=1) // false
+// console.log(2==2) // true
+// console.log(5!=3) // true
+// console.log(5=="5")  //true
+// console.log(5==="5") //false
+
+
+// console.log("2">1)// true
+// console.log("02">1) //true
+
+// console.log(null>0)
+// console.log(null === 0)
+// console.log(null < 0)
+
+
+
