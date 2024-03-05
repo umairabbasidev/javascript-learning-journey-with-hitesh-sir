0ef6c8e5b142840ee5bc9164924d138276e52cbf
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-05 16:18:05 +0500

    02_dataTypes complted, we covered dataType concept

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+
+// alert(1 + 2);  // we are using node js, not browser
+
+// DataTypes in javascript
+// Primitive datatype  NN SS BB U 
+let age = 21;
+let temp = null;  // stand alone value  TYPEOF onject
+let name = "Umair";
+// let symbol = Symbol(123);  // uniqueness 
+let isLoggedIn = true; // false
+let bigInt = 24778478487987348478974475887687n;
+let undefine = undefined;
+// console.log(age);
