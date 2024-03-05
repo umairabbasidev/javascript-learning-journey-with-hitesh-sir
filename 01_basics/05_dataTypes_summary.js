8f358284441387c38f1a566b2205b3581011a0d3
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-05 21:48:40 +0500

    finally completed datatypes in javascript with full of understanding

---
+++ 01_basics/05_dataTypes_summary.js
@@ -0,0 +1,56 @@
+// Catagorization of datatypes in js is two
+// one is primitive data types or call by values
+// non primitive or pass by reference
+
+// kis tarah data ko mermory main rekha jata h or access  kia jata h os basis pe datatypes ki two catagorization h
+
+// Primitive : Or calll by value
+// Non-primitive Or Reference Type
+
+// Primitive DataTypes:
+// 7 Types For analogy NN SS BB U
+// Null Number, String Symbol, Boolean BigInt , Undefined
+
+// let score = 100;
+// let scoreValue = 100.5;
+// let outSideTemp = null;
+
+// let id = Symbol(123);
+// let id2 = Symbol(123)
+// console.log(id===id2) // false
+// if(id ==id2){
+//  console.log("equal")
+// }else{
+//  console.log("not equal") // not equal
+// }
+// console.log(id,id2);
+// let str = "Umair"
+// let isLoggedIn = false;
+
+// let UserEmail;
+// let userMail = undefined;
+
+// Reference Types Non-Primitive
+// Array, object, function
+
+// const student = ['umair','noman','ali','marsad','farhan']
+// let studentOneInfo = {
+//  name:"Marsad",
+//  age:30,
+//  skill:"Designer",
+//  educattion:12
+// }
+
+// function basicFunction(){
+//  console.log("Hello function ")
+// }
+
+// basicFunction()
+
+// let functionInVarable = function(){
+//  console.log("Hello World")
+// }
+
+// functionInVarable()
+// console.log(typeof functionInVarable)
+
