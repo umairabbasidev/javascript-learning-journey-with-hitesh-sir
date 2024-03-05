3fda79b966b794b931270a6a47f77f87927878f0
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-05 20:14:00 +0500

    03_conversion and opertions in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,5 +31,35 @@
 // let numberInStr = String(someNumber)
 // console.log(numberInStr)
 // console.log(typeof numberInStr)
-
 // 33 = "33"
+
+
+// ************ Operations *****************
+
+// let value = 3;
+// let negValue = -value
+// console.log(negValue)
+
+/*console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2*2)
+console.log(2**3)
+console.log(3%2)*/
+
+// let str1 = "Umair";
+// let str2 = " Abbbasi"
+//  let result = str1+= str2
+//  console.log(result)
+
+// console.log('1' + 2) // 12
+// console.log(1 + '2') // 12
+
+// console.log('1' + 2 + 2) // 12 
+//  agr string first h to sub ko string treat kre ga 
+//  or agr string last mai h to phele jo calculations h wo ho gi then phir age side by side kr key likhe dia jay ga.
+
+// console.log(+true) // 1
+
+// console.log(+ "") // 0
+
