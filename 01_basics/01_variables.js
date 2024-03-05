a35ac4383bc1243df30c5daf6b629b91bfc0435c
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-05 10:47:04 +0500

    class 01_varable completd,we covered the concept of variable uing var,let,const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+console.log("Hello javascript");
+const accoundId = 144553;
+let accountEmail = "umair@google.com";
+var accountPassword = "12345";
+accountCity = "pakistan";
+let accountState;
+
+
+// accoundId = 2; // not allowed
+accountEmail = "umair@ai.com";
+accountPassword = "2121212";
+accountCity = "Turkey";
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and function scope
+*/
