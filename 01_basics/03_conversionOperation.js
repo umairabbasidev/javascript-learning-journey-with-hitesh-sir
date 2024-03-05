a6be09ce17dbf7ef901dbfaae5e9b6ef699205cd
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-05 18:29:46 +0500

    03_conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33";
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// "33" => 33
+// "33abc" => NaN typeof number
+// "umair" => "NaN"
+// true = > 1
+// false => 0
+// null => NAN
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// let isLoggedOut = 0;
+// let booleanIsLoggedOut = Boolean(isLoggedOut)
+// console.log(booleanIsLoggedOut)
+// console.log(typeof booleanIsLoggedOut)
+// let isLoggIn = ""
+// let booIsLoggedIn = Boolean(isLoggIn)
+// console.log(booIsLoggedIn)
+
+// 1 = > true 
+// 0 => false
+// "" => false
+//  "umair" => true
+
+// let someNumber = 33;
+// let numberInStr = String(someNumber)
+// console.log(numberInStr)
+// console.log(typeof numberInStr)
+
+// 33 = "33"
