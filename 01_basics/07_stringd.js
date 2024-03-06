d5a560eec2617591baccfd6c67c8ed7321029d89
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-06 10:30:03 +0500

    07_understanding string and some important methods

---
+++ 01_basics/07_stringd.js
@@ -0,0 +1,73 @@
+// const firstName = "Umair";
+// const lastName = "Abbasi";
+// console.log(firstName + lastName + ".");
+
+// console.log(`Hello my first name is ${firstName.toUpperCase()} and my last name is ${lastName.toUpperCase()}`)
+
+
+// let anotherStr = new String("umair");
+// console.log(anotherStr)
+// console.log(anotherStr.__proto__)
+
+
+
+let gameName = new String("IGI-I");
+console.log(gameName[0]);
+console.log(gameName.slice(0,5))
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.toLowerCase())
+console.log(gameName.startsWith("I"))
+console.log(gameName.endsWith('u'))
+console.log(gameName.indexOf('I')) // 0
+console.log(gameName.lastIndexOf('I')) // 4
+console.log(gameName.charAt(1)) // //G
+console.log(gameName)
+
+
+
+// charAt(index): Returns the character at the specified index.
+// let str = "Hello";
+// console.log(str.charAt(0)); // Output: "H"
+
+// concat(string2): Combines two or more strings.
+// let str1 = "Hello";
+// let str2 = "World";
+// console.log(str1.concat(" ", str2)); // Output: "Hello World"
+
+// indexOf(searchValue): Returns the index within the calling string object of the first occurrence of the specified value, starting the search at fromIndex.
+// let str = "Hello World";
+// console.log(str.indexOf("W")); // Output: 6
+
+
+// slice(start, end): Extracts a section of a string and returns it as a new string.
+// let str = "Hello World";
+// console.log(str.slice(0, 5)); // Output: "Hello"
+
+// toUpperCase(): Converts all characters in a string to uppercase.
+// let str = "hello";
+// console.log(str.toUpperCase()); // Output: "HELLO"
+
+// toLowerCase(): Converts all characters in a string to lowercase.
+// let str = "HELLO";
+// console.log(str.toLowerCase()); // Output: "hello"
+
+// replace(searchValue, replaceValue): Searches a string for a specified value or regular expression and returns a new string where the specified values are replaced.
+// let str = "Hello World";
+// console.log(str.replace("World", "Universe")); // Output: "Hello Universe"
+
+
+// trim(): Removes whitespace from both ends of a string.
+// let str = "   Hello World   ";
+// console.log(str.trim()); // Output: "Hello World"
+
+
+// split(separator): Splits a string into an array of substrings based on the specified separator.
+// let str = "Hello,World";
+// console.log(str.split(",")); // Output: ["Hello", "World"]
+
+
+// substring(start, end): Extracts the characters from a string between two specified indices.
+// let str = "Hello World";
+// console.log(str.substring(0, 5)); // Output: "Hello"x
+
