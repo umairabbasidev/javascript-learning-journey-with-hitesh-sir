0965bb2087357bd8d312dafc0cf8c5e00b648fad
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-12 15:42:28 +0500

    09_dates and time concept are completed

---
+++ 01_basics/09_dates_inJS.js
@@ -0,0 +1,29 @@
+let myDate = new Date()
+// console.log(myDate.toDateString())  // Tue Mar 12 2024
+// console.log(myDate.toISOString()) // 2024-03-12T09:22:51.833Z
+// console.log(myDate.toJSON()) // 2024-03-12T09:23:28.851Z
+// console.log(myDate.toLocaleDateString()) // 12/03/2024
+// console.log(myDate.toLocaleString()) // 12/03/2024, 14:24:29
+// console.log(myDate.toLocaleTimeString()) // 14:24:54
+// console.log(myDate.toString()) // 09_dates_inJS.js:2 Tue Mar 12 2024 14:20:04 GMT+0500 (Pakistan Standard Time)
+// console.log(myDate.toTimeString()) // 14:26:19 GMT+0500 (Pakistan Standard Time)
+// console.log(myDate.toUTCString()) // Tue, 12 Mar 2024 09:27:00 GMT
+// console.log(myDate.getDate())// 12
+// console.log(myDate.getDay()) // 2 starting 0 which sunday and 6 which is saturday
+// console.log(myDate.getFullYear()) // 2024
+// console.log(myDate.getHours()) // 14
+// console.log(myDate.getMinutes()) // 34
+// console.log(myDate.getSeconds()) // 30
+// console.log(myDate.getMonth()) //2
+// console.log(myDate.getTime())  // 1710236143878 return milliseconds staring from jan 1st 1970
+
+// // let date = new Date('01/1/2024')
+// let date = new Date(2024,0,23,5,6,9)
+
+// console.log(date.toDateString())  // Tue Jan 23 2024
+// console.log(date.toLocaleString())  // 23/01/2024, 05:06:09
+
+
+// let date = new Date('2023-01-14')  // YYMMDD
+// let date = new Date('01-14-2023-')  // MMDDYY
+// console.log(date.toLocaleString())
