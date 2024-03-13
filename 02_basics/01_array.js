e505c67a248c7a2b3f5072e1e51e254e742cf462
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-13 15:21:45 +0500

    02_basics, array and some import methods are complted

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,1 +1,40 @@
-console.log("Hello")
+// array  paranthesis(),suqare brakets[],curly braces{}
+
+// const myArr = [0, 1, 2, 3, 4, 5];
+// console.log(myArr[1]);
+
+// let arr = new Array(6,7,8,9)
+// console.log(arr)
+
+// array methods
+
+let arrays = [1, 2, 3, "umair"];
+// let push = arr.push(4,5)  // add elements in last and return new length of the array
+// let pop = arr.pop(); // remove element in last and return the removed element
+// let unshift = arr.unshift(0); // add elements in start and return new length of the array
+// let shift = arr.shift(); // remove element first and return the removed element
+
+// console.log( arrays.includes(2) ) // true
+// console.log(arrays.indexOf(1))
+// let newArr = arrays.join();
+// console.log(arrays) // (4) [1, 2, 3, 'umair']
+// console.log(newArr) //  1,2,3,umair
+
+// slice, splice
+
+// let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [6, 7, 8, 9, 10];
+
+// console.log("A ",arr1); // A [1,2,3,4,5]
+// const myN1 = arr1.slice(1,3);
+// console.log(myN1) // [2,3]
+// console.log("B " ,arr1) // B [1,2,3,4,5]
+
+// let myN2 = arr1.splice(1,3,9);  
+// console.log("splice",myN2)
+// console.log("A",arr1)
+// summary slice does't effect the original array while splice effect the original array
+
+let a = arr2.splice(1,3,"umair","abbasi")
+console.log(a)
+console.log(arr2)
