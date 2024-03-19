4f033fc45dc8638b012e6b3c019acce96bf697ac
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-19 11:00:45 +0500

    concept of for each loop are completed

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,7 +1,47 @@
 // forEach loop
 
-let coding = ["js","ruby","java","python","cpp"]
+let coding = ["js", "ruby", "java", "python", "cpp"];
 
-coding.forEach((item)=>{
-console.log(item)
-})
+// coding.forEach(function(val){
+// console.log(val)
+// })
+
+// let val = coding.forEach((item) => {
+//   console.log(item);
+// });
+
+// console.log(val) // undefined
+
+// first created  function then pass the reference  in foreach loop it is also fine
+// function printMe(item) {
+//   console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+let val = coding.forEach((item, index, array) => {
+  // console.log(item,index,array)
+  let data = item;
+  return data;
+});
+
+// console.log(val)  // undefined
+
+let myCoding = [
+  {
+    languageName: "javascript",
+    languageFileName: "javascript",
+  },
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "pyhton",
+    languageFileName: "py",
+  },
+];
+
+myCoding.forEach((item) => {
+  console.log(item.languageFileName);
+});
